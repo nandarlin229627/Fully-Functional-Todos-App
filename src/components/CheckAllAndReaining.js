7c72a417f9fa0b035ab8a064c8b8022648b2ce56
nandarlin229627 <64903581+nandarlin229627@users.noreply.github.com>
2023-04-27 18:17:16 +0630

    Api Setup and Components Extracation

---
+++ src/components/CheckAllAndReaining.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function CheckAllAndReaining() {
+  return (
+        <div className="check-all-container">
+          <div>
+            <div className="button">Check All</div>
+          </div>
+
+          <span>3 items remaining</span>
+        </div>
+  )
+}
