a8c2dec35180d22b4a30c0e7445ccf99aaf2e151
nandarlin229627 <64903581+nandarlin229627@users.noreply.github.com>
2023-04-27 18:40:38 +0630

    Fetch All Todos

--- src/App.js
+++ src/App.js
@@ -5,14 +5,26 @@ import TodoList from './components/TodoList';
 import CheckAllAndReaining from './components/CheckAllAndReaining';
 import TodoFilters from './components/TodoFilters';
 import ClearCompletedBtn from './components/ClearCompletedBtn';
+import { useEffect, useState } from 'react';
 
 function App() {
+
+  let [ todos ,setTodos ] = useState([]);
+
+  useEffect(() => {
+    fetch('http://localhost:3001/todos')
+    .then(res => res.json())
+    .then((todos) => {
+      setTodos(todos);
+    })
+  },[])
+
   return (
     <div className="todo-app-container">
       <div className="todo-app">
         <h2>Todo App</h2>
         <TodoForm/>
-        <TodoList/>
+        <TodoList todos = {todos}/>
         <CheckAllAndReaining/>
         <div className="other-buttons-container">         
           <TodoFilters/>
