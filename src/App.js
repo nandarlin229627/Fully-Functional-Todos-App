6c1002144ec2c0aaebaebcf716d5901dc4f18d54
nandarlin229627 <64903581+nandarlin229627@users.noreply.github.com>
2023-05-19 18:03:07 +0630

    first

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import './reset.css';
 import './App.css';
+<<<<<<< HEAD
 import TodoForm from './components/TodoForm';
 import TodoList from './components/TodoList';
 import CheckAllAndReaining from './components/CheckAllAndReaining';
 import TodoFilters from './components/TodoFilters';
 import ClearCompletedBtn from './components/ClearCompletedBtn';
 import { useEffect, useState } from 'react';
+=======
+>>>>>>> parent of 7c72a41 (Api Setup and Components Extracation)
 
 function App() {
 
@@ -23,12 +26,113 @@ function App() {
     <div className="todo-app-container">
       <div className="todo-app">
         <h2>Todo App</h2>
+<<<<<<< HEAD
         <TodoForm/>
         <TodoList todos = {todos}/>
         <CheckAllAndReaining/>
         <div className="other-buttons-container">         
           <TodoFilters/>
           <ClearCompletedBtn/>
+=======
+        <form action="#">
+          <input
+            type="text"
+            className="todo-input"
+            placeholder="What do you need to do?"
+          />
+        </form>
+
+        <ul className="todo-list">
+          <li className="todo-item-container">
+            <div className="todo-item">
+              <input type="checkbox" />
+              <span className="todo-item-label">Finish React Series</span>
+              {/* <input type="text" className="todo-item-input" value="Finish React Series" /> */}
+            </div>
+            <button className="x-button">
+              <svg
+                className="x-button-icon"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth={2}
+                  d="M6 18L18 6M6 6l12 12"
+                />
+              </svg>
+            </button>
+          </li>
+          <li className="todo-item-container">
+            <div className="todo-item">
+              <input type="checkbox" />
+              <span className="todo-item-label line-through">
+                Go to Grocery
+              </span>
+              {/* <input type="text" className="todo-item-input" value="Go to Grocery" /> */}
+            </div>
+            <button className="x-button">
+              <svg
+                className="x-button-icon"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth={2}
+                  d="M6 18L18 6M6 6l12 12"
+                />
+              </svg>
+            </button>
+          </li>
+          <li className="todo-item-container">
+            <div className="todo-item">
+              <input type="checkbox" />
+              <span className="todo-item-label">Do other thing</span>
+              {/* <input type="text" className="todo-item-input" value="Do other thing /> */}
+            </div>
+            <button className="x-button">
+              <svg
+                className="x-button-icon"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth={2}
+                  d="M6 18L18 6M6 6l12 12"
+                />
+              </svg>
+            </button>
+          </li>
+        </ul>
+
+        <div className="check-all-container">
+          <div>
+            <div className="button">Check All</div>
+          </div>
+
+          <span>3 items remaining</span>
+        </div>
+
+        <div className="other-buttons-container">
+          <div>
+            <button className="button filter-button filter-button-active">
+              All
+            </button>
+            <button className="button filter-button">Active</button>
+            <button className="button filter-button">Completed</button>
+          </div>
+          <div>
+            <button className="button">Clear completed</button>
+          </div>
+>>>>>>> parent of 7c72a41 (Api Setup and Components Extracation)
         </div>
       </div>
     </div>
--- src/components/ClearCompletedBtn.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function ClearCompletedBtn() {
-  return (
-    <div>
-        <button className="button">Clear completed</button>
-  </div>
-  )
-}
